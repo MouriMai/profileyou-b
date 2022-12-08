bbe210da714638f9bd2a5872b540b5b613089abb
MouriMai <mori-ma@ibjapan.jp>
2022-12-08 19:36:42 +0900

    react in progress

--- src/App.js
+++ src/App.js
@@ -15,25 +15,107 @@ import {
 }
   from "@chakra-ui/react"
 import axios from "axios";
-import { useState } from "react";
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 import Keywords from "./components/Keywords";
+
 
 const App = () => {
   const [image, updateImage] = useState();
   const [prompt, updatePrompt] = useState();
   const [loading, updateLoading] = useState();
-  const [keywords, updateKeywords] = useState();
+  const [keywords, setKeywords] = useState([]);
 
-  const addKeywordHandler = keyword => {
-    updateKeywords([keyword, ...keywords])
+
+  // START FETCHING
+  useEffect(() => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    }
+
+    fetch(`http://localhost:8080/keywords`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setKeywords(data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+  // FINISH FETCHING
+
+  // DELETE
+  function confirmDelete(id) {
+    const url = `http://localhost:8080/keyword/delete/${id}`;
+
+    Swal.fire({
+      title: 'Delete keyword?',
+      text: "You cannot undo this action!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // axios.delete(url)
+        // .then(res => {
+        //   const keywords = this.state.keywords.filter(keyword => keyword.id !== id);
+        //   console.log("Delete from react:");
+        //   setKeywords({keywords})
+        //   console.log(res.data);
+        // })
+        let headers = new Headers();
+        // headers.append("Authorization", "Bearer " + jwtToken)
+        const requestOptions = {
+          method: "DELETE",
+          headers: headers,
+        };
+
+        const url = `http://localhost:8080/keyword/delete/${id}`;
+        console.log(url);
+        fetch(`/keyword/delete/${id}`, requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.error) {
+              console.log("Something's Error");
+              console.log(data.error);
+            } else {
+              console.log("Finish");
+              setKeywords(data);
+              // navigate("/keywords");
+            }
+          })
+          .catch(err => {
+            console.log("Error was caught:");
+            console.log(err);
+          });
+      }
+    });
   }
+    // FINISH DELETE
+
+
+  const addKeywordHandler = (keyword) => {
+    // event.preventDefault()
+    console.log(keywords);
+    setKeywords([...keywords, keyword]);
+    console.log(keyword);
+  };
 
   const generate = async (prompt) => {
+    addKeywordHandler(prompt)
     updateLoading(true);
     const request = await axios.post(`http://localhost:8080/keyword/create/${prompt}`);
-    const result = await axios.get(`http://localhost:8080/keyword/create/${prompt}`);
-    console.log(result, request);
-    updateImage(result.data);
+    // const result = await axios.get(`http://localhost:8080/keyword/create/${prompt}`);
+    console.log(request);
+    // console.log(result);
+    // updateImage(result.data);
     updateLoading(false);
   };
 
@@ -57,7 +139,10 @@ const App = () => {
             onChange={(e) => updatePrompt(e.target.value)}
             width={"350px"}
           ></Input>
-          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+          <Button onClick={(e) => {
+            e.preventDefault()
+            generate(prompt)
+            }} colorScheme={"yellow"}>
             Generate
           </Button>
         </Wrap>
@@ -73,7 +158,7 @@ const App = () => {
 
         <pre>{JSON.stringify(prompt)}</pre>
 
-        <Keywords />
+        <Keywords keywords={keywords} confirmDelete={confirmDelete}/>
 
       </Container>
     </ChakraProvider>
--- src/components/Keywords.js
+++ src/components/Keywords.js
@@ -2,87 +2,13 @@ import { useEffect, useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { Link, redirect } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
-import Swal from "sweetalert2";
 import axios from 'axios'
 
 const Keywords = (props) => {
   // const navigate = useNavigate();
   const [keywords, setKeywords] = useState([]);
 
-  useEffect(() => {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
 
-    const requestOptions = {
-      method: "GET",
-      headers: headers,
-    }
-
-    fetch(`http://localhost:8080/keywords`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setKeywords(data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-  }, []);
-
-const confirmDelete = (id) => {
-  const url = `http://localhost:8080/keyword/delete/${id}`
-
-    Swal.fire({
-        title: 'Delete keyword?',
-        text: "You cannot undo this action!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-            // axios.delete(url)
-            // .then(res => {
-            //   const keywords = this.state.keywords.filter(keyword => keyword.id !== id);
-
-            //   console.log("Delete from react:");
-            //   setKeywords({keywords})
-
-            //   console.log(res.data);
-            // })
-
-            let headers = new Headers();
-            // headers.append("Authorization", "Bearer " + jwtToken)
-
-            const requestOptions = {
-                method: "DELETE",
-                headers: headers,
-            }
-
-            console.log("Fetch starting!");
-            const url = `http://localhost:8080/keyword/delete/${id}`
-            console.log(url);
-          fetch(`keyword/delete/${id}`, requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.error) {
-                  console.log("Something's Error");
-                  console.log(data.error);
-                } else {
-                  console.log("Finish");
-                  setKeywords(data);
-                    // navigate("/keywords");
-                }
-            })
-            .catch(err => {
-              console.log("Error was caught:");
-              console.log(err)
-            });
-        }
-      })
-  }
 
   return (
     <div>
@@ -97,7 +23,7 @@ const confirmDelete = (id) => {
           </tr>
         </thead>
         <tbody>
-          {keywords.map((k, index) => (
+          {props.keywords.map((k, index) => (
             <tr key={index}>
               <td>{k.Word}
                 {/* <Link to={`/keywords/${k.ID}`}>
@@ -115,7 +41,7 @@ const confirmDelete = (id) => {
 
               </td>
               <td>{k.CreatedAt}</td>
-              <td><Button onClick={() => confirmDelete(k.ID)} colorScheme={"gray"}  variant={"outline"}>
+              <td><Button onClick={() => props.confirmDelete(k.ID)} colorScheme={"gray"}  variant={"outline"}>
             🗑️ Delete
           </Button></td>
             </tr>
