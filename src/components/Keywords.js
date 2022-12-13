6ad1e11c7be3032310465af203ab027f48c06cbc
“MouriMai” <mori-ma@ibjapan.jp>
2022-12-13 15:53:06 +0900

    usecase: Keywords.js update

--- src/components/Keywords.js
+++ src/components/Keywords.js
@@ -1,15 +1,10 @@
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { Link, redirect } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
 import { format, formatDistance, formatRelative, subDays } from 'date-fns'
 import { json } from "react-router-dom";
 // import axios from 'axios'
 
 const Keywords = (props) => {
-  // const navigate = useNavigate();
-  // const [keywords, setKeywords] = useState([]);
-
 
 
   return (
@@ -27,11 +22,8 @@ const Keywords = (props) => {
         <tbody>
           {props.keywords.map((k, index) => (
             <tr key={index}>
-              <td>{k.Word}
-                {/* <Link to={`/keywords/${k.ID}`}>
-                                    {k.word}
-                                </Link> */}
-              </td>
+              <td><Link to={`/keywords/${k.ID}`}>{k.Word}</Link></td>
+              
               <td>{k.Description}</td>
               <td>
                 {k.ImageUrl !== "" &&
--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import Keywords from './components/Keywords';
+import Keyword from './components/Keyword';
+import Login from './components/Login';
+import Home from './components/Home.js';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home /> },
+      {
+        path: "/keywords",
+        element: <Keywords />,
+      },
+      {
+        path: "/keywords/:id",
+        element: <Keyword />,
+      },
+      // {
+      //   path: "/admin/movie/0",
+      //   element: <EditMovie />,
+      // },
+      // {
+      //   path: "/admin/movie/:id",
+      //   element: <EditMovie />,
+      // },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ]
+  }
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+      {/* <RouterProvider router={router} /> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 
